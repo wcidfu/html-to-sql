e40b9cc0aca2b0bc8d8a378e0eb9ae54a0de0d20
mby <michaelpushcof@gmail.com>
2024-12-01 19:10:50 +0300

    new :pig:

--- parseTest.js
+++ parseTest.js
@@ -1,21 +1,18 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
 
-// Чтение HTML-кода из файла
+// html
 const htmlData = fs.readFileSync('htmlCode.txt', 'utf8');
 
-// Разделение на отдельные тесты
 const tests = htmlData.split('###').filter(test => test.trim());
 
 let resultData = '';
 
 tests.forEach((testHtml, index) => {
     const $ = cheerio.load(testHtml);
-    
-    // Извлечение вопроса
+  
     const question = $('.otp-item-view-title a').text().trim();
 
-    // Извлечение ответов
     let answers = [];
     $('.rb-chk-container .item').each((i, el) => {
         const answerText = $(el).find('p').text().trim();
@@ -23,14 +20,14 @@ tests.forEach((testHtml, index) => {
         answers.push({ answerText, isCorrect });
     });
 
-    // Формирование SQL-запросов
-    resultData += `-- Тест ${index + 1}\n`; // Добавление разделителя
+    // sql
+    resultData += `-- Тест ${index + 1}\n`;
     answers.forEach(answer => {
         resultData += `INSERT INTO answers (question, answer, is_correct) VALUES ('${question}', '${answer.answerText}', ${answer.isCorrect});\n`;
     });
-    resultData += `\n-- Конец теста ${index + 1}\n\n`; // Завершение разделителя
+    resultData += `\n-- Конец теста ${index + 1}\n\n`;
 });
 
-// Запись результатов в файл
+// result
 fs.writeFileSync('sqlResult.txt', resultData, 'utf8');
-console.log('Результаты успешно записаны в sqlResult.txt');
+console.log('all fine => sqlResult');
