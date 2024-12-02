1e390bbf5cc6b3664735f46e08568be7f86765d2
mby <michaelpushcof@gmail.com>
2024-12-02 06:49:24 +0300

    new :pig:

--- parseTest.js
+++ parseTest.js
@@ -3,31 +3,29 @@ const cheerio = require('cheerio');
 
 // html
 const htmlData = fs.readFileSync('htmlCode.txt', 'utf8');
-
 const tests = htmlData.split('###').filter(test => test.trim());
 
 let resultData = '';
 
 tests.forEach((testHtml, index) => {
     const $ = cheerio.load(testHtml);
-  
+    
     const question = $('.otp-item-view-title a').text().trim();
 
-    let answers = [];
+    resultData += `-- Test ${index + 1}\n`;
+    resultData += `INSERT INTO TEST (question) VALUES ('${question}');\n`;
+
     $('.rb-chk-container .item').each((i, el) => {
         const answerText = $(el).find('p').text().trim();
-        const isCorrect = $(el).find('.icon-rb-checked').length > 0; // Проверка правильности ответа
-        answers.push({ answerText, isCorrect });
+        const isCorrect = $(el).find('.icon-rb-checked').length > 0; 
+        
+        // sql
+        resultData += `INSERT INTO ANSWER_OPTIONS (test_id, answer, is_correct) VALUES (currval('test_id_seq'), '${answerText}', ${isCorrect});\n`;
     });
 
-    // sql
-    resultData += `-- Тест ${index + 1}\n`;
-    answers.forEach(answer => {
-        resultData += `INSERT INTO answers (question, answer, is_correct) VALUES ('${question}', '${answer.answerText}', ${answer.isCorrect});\n`;
-    });
-    resultData += `\n-- Конец теста ${index + 1}\n\n`;
+    resultData += `\n\n\n`;
 });
 
-// result
+// => sqlResult
 fs.writeFileSync('sqlResult.txt', resultData, 'utf8');
-console.log('all fine => sqlResult');
+console.log('all fine => sqlResult.txt');
